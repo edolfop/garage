92b503cda5de15761d332e355329650c671f8be4
edolfop <alfonsojpg@gmail.com>
2023-08-29 14:45:59 +0200

    comienzo de los graficos en navegador

--- prueba.py
+++ prueba.py
@@ -1,10 +1,40 @@
-from flask import Flask
+import tkinter as tk
+from tkinter import ttk
+import sqlite3
 
-app = Flask(__name__)
+class TableApp:
+    def __init__(self, root, db_connection):
+        self.root = root
+        self.db_connection = db_connection
+        self.root.title("Tabla")
+        
+        # Configura el tamaño de la ventana para que ocupe toda la pantalla
+        self.root.attributes("-fullscreen", True)
 
-@app.route('/')
-def hello():
-    return "¡Hola desde la Raspberry Pi!"
+        # Calcula las dimensiones y la posición del centro de la pantalla
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        table_width = 800
+        table_height = 600
+        x_position = (screen_width - table_width) // 2
+        y_position = (screen_height - table_height) // 2
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+        self.table = ttk.Treeview(root)
+        self.table["columns"] = ("Marca", "Modelo", "Año", "Placa", "Nombre", "Apellido", "Dirección", "Teléfono")
+        # ... (configuración de columnas, encabezados, etc.)
+
+        # Establece las dimensiones y posición de la tabla en el centro
+        self.table.place(x=x_position, y=y_position, width=table_width, height=table_height)
+
+        self.insert_data()
+
+    # ... (método insert_data y otros métodos)
+
+def main():
+    db_connection = sqlite3.connect("datos.db")  # Cambia "datos.db" por el nombre de tu base de datos
+    root = tk.Tk()
+    app = TableApp(root, db_connection)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
